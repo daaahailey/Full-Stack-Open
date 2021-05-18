6e3cc21e988604b5528ab0406d9457b11896b391
daaahailey <56154510+daaahailey@users.noreply.github.com>
2021-05-18 11:39:19 +0100

    exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import DisplayPeople from "./components/DisplayPeople";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456" },
-    { name: "Ada Lovelace", number: "39-44-5323523" },
-    { name: "Dan Abramov", number: "12-43-234345" },
-    { name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filterName, setFilterName] = useState("");
+
+  useEffect(() => {
+    // console.log("effect");
+    axios.get("http://localhost:3001/persons").then((response) => {
+      // console.log("promise fulfilled");
+      // console.log(response.data);
+      setPersons(response.data);
+    });
+  }, []);
+  // console.log("render", persons.length, "persons");
 
   const addNewPerson = (event) => {
     event.preventDefault();
